598bd2410258edcd6f86715cf95609181a5da8ed
javiershaka <javiershaka@gmail.com>
2019-05-22 20:59:28 -0700

    se modifico el main layout

--- app/src/main/java/mx/itson/pastel/MainActivity.java
+++ app/src/main/java/mx/itson/pastel/MainActivity.java
@@ -1,17 +1,9 @@
 package mx.itson.pastel;
-
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import mx.itson.pastel.entidades.Usuario;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,42 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final EditText etNombre = findViewById(R.id.etNombre);
-        final EditText etCorreo = findViewById(R.id.etCorreo);
-        final EditText etTelefono = findViewById(R.id.etTelefono);
-        findViewById(R.id.btnGuardar).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!etNombre.getText().toString().isEmpty() &&
-                        !etCorreo.getText().toString().isEmpty() &&
-                        !etTelefono.getText().toString().isEmpty()){
-                    Usuario u = new Usuario(getApplicationContext());
-                    u.guardar(etNombre.getText().toString(),etTelefono.getText().toString(), etCorreo.getText().toString());
-                    actualizarLista();
-                    etNombre.setText("");
-                    etCorreo.setText("");
-                    etTelefono.setText("");
-                    List<Usuario> usuarios = new Usuario(getApplicationContext()).getAll();
-                    Toast.makeText(getApplicationContext(),
-                            "Se añadió correctamente: " + usuarios.get(usuarios.size()-1).getNombre(), Toast.LENGTH_LONG).show();
-                }else{
-                    Toast.makeText(getApplicationContext(), "Faltan datos", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-        actualizarLista();
     }
 
-    private void actualizarLista() {
-        List<Usuario> usuarios = new Usuario(getApplicationContext()).getAll();
-        ListView lista = findViewById(R.id.lvLista);
-        List<String> nombres = new ArrayList<>();
-        for (Usuario u : usuarios){
-            nombres.add(u.getNombre());
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1,
-                nombres);
-        lista.setAdapter(adapter);
+    public void Llamar(View view){
+        Intent llamar = new Intent(this, DeleteUserActivity.class);
+        startActivity(llamar);
+
+    }
+    public void Camara(View view){
+        Intent cam = new Intent(this, EditUserActivity.class);
+        startActivity(cam);
+
+    }
+    public void Sms(View view){
+        Intent sms = new Intent(this, NewUserActivity.class);
+        startActivity(sms);
+
     }
 }
--- app/src/main/java/mx/itson/pastel/NewUserActivity.java
+++ app/src/main/java/mx/itson/pastel/NewUserActivity.java
@@ -2,6 +2,16 @@ package mx.itson.pastel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mx.itson.pastel.entidades.Usuario;
 
 public class NewUserActivity extends AppCompatActivity {
 
@@ -9,5 +19,42 @@ public class NewUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+        final EditText etNombre = findViewById(R.id.etNombre);
+        final EditText etCorreo = findViewById(R.id.etCorreo);
+        final EditText etTelefono = findViewById(R.id.etTelefono);
+        findViewById(R.id.btnGuardar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!etNombre.getText().toString().isEmpty() &&
+                        !etCorreo.getText().toString().isEmpty() &&
+                        !etTelefono.getText().toString().isEmpty()){
+                    Usuario u = new Usuario(getApplicationContext());
+                    u.guardar(etNombre.getText().toString(),etTelefono.getText().toString(), etCorreo.getText().toString());
+                    actualizarLista();
+                    etNombre.setText("");
+                    etCorreo.setText("");
+                    etTelefono.setText("");
+                    List<Usuario> usuarios = new Usuario(getApplicationContext()).getAll();
+                    Toast.makeText(getApplicationContext(),
+                            "Se añadió correctamente: " + usuarios.get(usuarios.size()-1).getNombre(), Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Faltan datos", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        actualizarLista();
+    }
+
+    private void actualizarLista() {
+        List<Usuario> usuarios = new Usuario(getApplicationContext()).getAll();
+        ListView lista = findViewById(R.id.lvLista);
+        List<String> nombres = new ArrayList<>();
+        for (Usuario u : usuarios){
+            nombres.add(u.getNombre());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                nombres);
+        lista.setAdapter(adapter);
     }
 }
